53f86a27019da2986c4a7154a45c07033d20262f
Yuki Watanabe <ukwhatn@gmail.com>
2023-01-29 03:20:53 +0900

    ロール・nick処理最適化

--- bot/cogs/UserManage.py
+++ bot/cogs/UserManage.py
@@ -19,6 +19,8 @@ class UserManage(commands.Cog):
 
         self.guild = None
 
+        self.notified_not_found_users = []
+
     @commands.Cog.listener("on_ready")
     async def on_ready(self):
         self.guild = self.bot.get_guild(int(os.getenv("DISCORD_GUILD_ID")))
@@ -33,7 +35,12 @@ class UserManage(commands.Cog):
             await member.add_roles(role)
             self.logger.info(f"Added {member.name} to {role.name}")
 
-    @tasks.loop(minutes=1)
+    async def remove_role(self, member: discord.Member, role: discord.Role):
+        if role in member.roles:
+            await member.remove_roles(role)
+            self.logger.info(f"Removed {member.name} from {role.name}")
+
+    @tasks.loop(seconds=15)
     async def update_users(self):
 
         if self.guild is None:
@@ -49,57 +56,144 @@ class UserManage(commands.Cog):
 
         for member in members:
 
-            if member.is_supporter:
-                continue
-
             if member.discord_user_id is not None:
-                discord_user = await self.guild.fetch_member(member.discord_user_id)
+                try:
+                    discord_user = await self.guild.fetch_member(member.discord_user_id)
+                except discord.NotFound:
+                    discord_user = None
+
                 if discord_user is not None and not discord_user.bot:
 
-                    if member.name_first is None or member.name_last is None:
-                        await self.add_role(
-                            discord_user,
-                            self.guild.get_role(int(os.getenv("DISCORD_NOT_REGISTERED_ROLE")))
-                        )
-                        correct_user_nane = None
-
-                    else:
-                        correct_user_nane = f"{member.name_first} {member.name_last}"
-
+                    # check manage
+                    is_manage = False
+                    manage_role = self.guild.get_role(int(os.getenv("DISCORD_MANAGE_ROLE")))
                     if member.is_admin:
-                        correct_user_nane += "_KC3運営"
-
-                    if member.is_supporter:
+                        is_manage = True
                         await self.add_role(
                             discord_user,
-                            self.guild.get_role(int(os.getenv("DISCORD_SUPPORTER_ROLE")))
+                            manage_role
+                        )
+                    else:
+                        await self.remove_role(
+                            discord_user,
+                            manage_role
                         )
 
-                    if correct_user_nane is not None and discord_user.nick != correct_user_nane:
-                        await discord_user.edit(nick=correct_user_nane)
-                        logging.info(f"Updated {discord_user.name}'s nickname to {correct_user_nane}")
+                    # check supporter
+                    supporter_role = self.guild.get_role(int(os.getenv("DISCORD_SUPPORTER_ROLE")))
+                    member_role = self.guild.get_role(int(os.getenv("DISCORD_MEMBER_ROLE")))
+                    is_supporter = False
+                    if member.is_supporter:
+                        is_supporter = True
+                        await self.add_role(
+                            discord_user,
+                            supporter_role
+                        )
+                        await self.remove_role(
+                            discord_user,
+                            member_role
+                        )
+                    else:
+                        await self.remove_role(
+                            discord_user,
+                            supporter_role
+                        )
+                        if not is_manage:
+                            await self.add_role(
+                                discord_user,
+                                member_role
+                            )
+                        else:
+                            await self.remove_role(
+                                discord_user,
+                                member_role
+                            )
 
+                    # 詳細情報登録チェック
+                    has_detailed_info = True
+                    has_not_detailed_role = self.guild.get_role(int(os.getenv("DISCORD_NOT_REGISTERED_ROLE")))
+                    if member.name_first is None or member.name_last is None:
+                        has_detailed_info = False
+                        await self.add_role(
+                            discord_user,
+                            has_not_detailed_role
+                        )
+                    else:
+                        await self.remove_role(
+                            discord_user,
+                            has_not_detailed_role
+                        )
+
+                    # GitHub登録チェック
+                    is_not_github_registered_role = self.guild.get_role(
+                        int(os.getenv("DISCORD_NOT_GITHUB_REGISTERED_ROLE")))
+                    if member.github_user_id is None:
+                        await self.add_role(
+                            discord_user,
+                            is_not_github_registered_role
+                        )
+                    else:
+                        await self.remove_role(
+                            discord_user,
+                            is_not_github_registered_role
+                        )
+
+                    # チームIDチェック
+                    has_team_id = False
                     if member.team_id is not None:
-                        team_name = f"チーム{member.team_id}"
+                        has_team_id = True
 
+                    # nickname構築
+                    nickname = ""
+
+                    if not is_supporter:
+                        if has_detailed_info:
+                            nickname += f"{member.name_first} {member.name_last}"
+                        else:
+                            nickname += f"{discord_user.name}_未登録"
+
+                        if is_manage:
+                            nickname += "_KC3運営"
+
+                        if has_team_id:
+                            nickname += f"_チーム{member.team_id}"
+
+                        if discord_user.nick != nickname:
+                            await discord_user.edit(nick=nickname)
+                            logging.info(f"Updated {discord_user.name}'s nickname to {nickname}")
+
+                    # チームロール付与処理
+                    if member.team_id is not None:
+                        team_role_name = f"チーム{member.team_id}"
                         is_correct_team_role_exist = False
 
                         for exist_role in discord_user.roles:
                             if exist_role.name in roles:
-                                if exist_role.name != team_name:
+                                if exist_role.name != team_role_name:
                                     await discord_user.remove_roles(exist_role)
                                     logging.info(f"Removed {exist_role.name} from {discord_user.name}")
                                 else:
                                     is_correct_team_role_exist = True
 
                         if not is_correct_team_role_exist:
-                            await self.add_role(discord_user, roles[team_name])
+                            await self.add_role(discord_user, roles[team_role_name])
+
+                    else:
+                        for exist_role in discord_user.roles:
+                            if exist_role.name in roles:
+                                await discord_user.remove_roles(exist_role)
+                                logging.info(f"Removed {exist_role.name} from {discord_user.name}")
 
                 else:
-                    await bot_config.NOTIFY_TO_OWNER(
-                        self.bot,
-                        f"User {member.name_first} {member.name_last} is not in the server")
+                    if member.id not in self.notified_not_found_users:
+                        # ユーザーがサーバーにいない場合は通知
+                        await bot_config.NOTIFY_TO_OWNER(
+                            self.bot,
+                            f"User {member.name_first} {member.name_last} "
+                            f"({member.discord_user_name}) is not in the server")
+                        self.notified_not_found_users.append(member.id)
 
+        # サーバーにいるがDBに登録されていないユーザーをキック
         members_discord_id = [member.discord_user_id for member in members]
         for discord_user in self.guild.members:
             if discord_user.bot:
